02a242e68c675255065af762ccc92cafd72f1cdc
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-11 18:42:52 -0500

    User controller - signUp - create user methos

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+
+    if User.find_by(email: user.email)
+      redirect_to '/signup'
+    else
+      if user.save
+        session[:user_id] = @user.id
+        redirect_to :home, notice: 'Account created successfully'
+      else
+        flash[:error] = 'An error occured!'
+        render 'new'
+      end
+    end
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  
+end
