6cb2bd0dec7acbfaa11004e5d3e6644ce7e55d20
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-15 16:48:21 -0500

    Signup error fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,18 +5,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    if User.find_by(email: @user.email)
-      redirect_to signup_path
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path, notice: 'Sign up successful!'
     else
-      if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path, notice: 'Sign up successful!'
-      else
-        render :new  
-      end
+      render :new  
     end
-
   end
 
   private
